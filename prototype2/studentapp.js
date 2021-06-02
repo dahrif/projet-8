75b1b6e4e5868032091f0b2c9b7f1467500aada2
dahrif <dahri.fatima.dev@gmail.com>
2021-06-02 12:07:56 +0100

    btn

--- prototype2/studentapp.js
+++ prototype2/studentapp.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
           studentsList = JSON.parse(data);
           console.log(studentsList);
           for (var i = 0; i < studentsList.length; i++) {
-            $(".studentsList").append('<table border=1><tr><td>'+"Name: "+studentsList[i]["name"]+'</td><td>'+"FatherName: "+studentsList[i]["fathername"]+'</td><td>'+"Rollno: "+studentsList[i]["rollno"]+'</td><td>'+"Degree: "+studentsList[i]["degree"]+'</td><td>'+"Branch: "+studentsList[i]["branch"]+'</td></tr><span class="delete-btn">delete</span><input type="hidden" value="" class="stdID"></table>');
+            $(".studentsList").append('<table border=1><tr><td>'+"Name: "+studentsList[i]["name"]+'</td><td>'+"FatherName: "+studentsList[i]["fathername"]+'</td><td>'+"Rollno: "+studentsList[i]["rollno"]+'</td><td>'+"Degree: "+studentsList[i]["degree"]+'</td><td>'+"Branch: "+studentsList[i]["branch"]+'</td></tr></table>');
           }
         }
       })
