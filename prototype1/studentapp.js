7c4059de79ed61bbbf0d867993177a5eac26cee4
dahrif <dahri.fatima.dev@gmail.com>
2021-06-04 10:43:07 +0100

    update/delete

--- prototype1/studentapp.js
+++ prototype1/studentapp.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         success:function(data) {
           studentsList = JSON.parse(data);
           console.log(studentsList);
-          for (var i = 0; i < studentsList.length; i++) {
+          for (var i = 0 ; i < studentsList.length; i++) {
             $(".studentsList").append('<table border=1><tr><td>'+"Name: "+studentsList[i]["name"]+'</td><td>'+"FatherName: "+studentsList[i]["fathername"]+'</td><td>'+"Rollno: "+studentsList[i]["rollno"]+'</td><td>'+"Degree: "+studentsList[i]["degree"]+'</td><td>'+"Branch: "+studentsList[i]["branch"]+'</td></tr></table>');
           }
         }
